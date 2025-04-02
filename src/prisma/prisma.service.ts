5a2a124ff8cb5c994044485ea9df95c906a06eff
luigikk <luigimee15@gmail.com>
2025-04-02 20:21:56 -0300

    ⚙️chore: prisma config

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, type OnModuleDestroy, type OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  public client: PrismaClient
+
+  constructor(){
+    super({
+      log: ['query']
+    })
+  }
+  onModuleInit() {
+    return this.$connect()
+  }
+  onModuleDestroy() {
+    return this.$disconnect()
+  }
+}
